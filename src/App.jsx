88479a1e90cb2b0db658be900086e62cbc7cf684
sinemsevimlikurt <sevimlikurtsinem@gmail.com>
2024-12-27 14:44:47 +0300

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from '../components/Login';
+import Success from '../components/Success';
+
 
 function App() {
-  
-
   return (
-    <>
-      
-    </>
-  )
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route path="/success" component={Success} />
+      </Switch>
+    </Router>
+  );
 }
 
-export default App
+export default App;
