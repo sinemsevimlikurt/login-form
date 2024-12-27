f3a9a47b5e13afd0b4f4ea9f57846750454a64b2
sinemsevimlikurt <sevimlikurtsinem@gmail.com>
2024-12-27 14:18:56 +0300

    cypress imp

---
+++ src/components/Success.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Alert } from 'reactstrap';
+
+const Success = () => {
+    return (
+        <>
+            <Alert>
+                This is a primary alert — check it out!
+            </Alert>
+            <h1>Login Successful!</h1>
+        </>
+    );
+};
+
+export default Success;
